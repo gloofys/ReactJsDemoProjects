0a8b2ffd1ff06e4fd7d336e3495467149a141afb
fredb <fred.brosman06@gmail.com>
2025-02-09 13:48:29 +0200

    Counter, Todo and Meals. Jsx demos.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Counter from "./components/Counter.jsx";
+// import Todo from "./components/Todo.jsx";
+import Meals from "./components/Meals.jsx";
+
+
+const App = () => {
+    return (
+        // <Counter/>
+        // <Todo/>
+        <Meals/>
+    )
+}
+export default App
---
+++ src/components/Meals.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+import"../styleMeals.css"
+
+const Meals = () => {
+    const[items, setItems] = useState([])
+
+    useEffect(() => {
+        axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(res=>
+            {
+                // console.log(res.data.meals)
+                setItems(res.data.meals);
+            })
+            .catch(err => console.log(err));
+    },[])
+
+    const itemsList = items.map(({strMeal, strMealThumb, idMeal}) => {
+        return (
+            <section className="card" key={idMeal}>
+              <img src={strMealThumb} alt="Meals" />
+                <section className="content">
+                    <p>{strMeal}</p>
+                    <p>#{idMeal}</p>
+                </section>
+            </section>
+        )
+    })
+
+    return (
+        <div className="items-container">{itemsList}</div>
+    )
+}
+export default Meals
